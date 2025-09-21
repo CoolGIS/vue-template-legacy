22bed0aaea9849b5f765efd56a44841a48db29fc
CoolGIS <mitooo@outlook.com>
2025-09-21 14:14:11 +0800

    refactor: 重构动画组件样式类组织形式
    (cherry picked from commit e29c63bc8de52bdf76d6672707a483ae207ef2df)

--- src/components/_transition/Fade.vue
+++ src/components/_transition/Fade.vue
@@ -29,7 +29,7 @@ interface TransitionProps {
 const {
   direction = 'fade',
   appear = false,
-  duration = '350ms',
+  duration = '300ms',
   easing = 'ease-in-out',
   delay = '0ms',
   translateX = '100%',
@@ -43,197 +43,112 @@ const {
   </Transition>
 </template>
 
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity v-bind(duration) v-bind(easing) v-bind(delay);
+<style scoped>
+[class*='-enter-active'],
+[class*='-leave-active'] {
+  transition:
+    transform v-bind(duration) v-bind(easing) v-bind(delay),
+    opacity v-bind(duration) v-bind(easing) v-bind(delay);
 }
-.fade-enter-from,
-.fade-leave-to {
+
+[class*='-enter-from'],
+[class*='-leave-to'] {
   opacity: 0;
 }
 
-.top-reverse-enter-active,
-.top-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .top-reverse-enter-from,
 .top-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(0, calc(v-bind(translateY) * -1), 0);
 }
 
-.top-enter-active,
-.top-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .top-enter-from {
-  opacity: 0;
   transform: translate3d(0, calc(v-bind(translateY) * -1), 0) scale3d(0.3, 0.3, 0.3);
 }
 .top-leave-to {
-  opacity: 0;
   transform: translate3d(0, v-bind(translateY), 0) scale3d(0.3, 0.3, 0.3);
 }
 
-.bottom-reverse-enter-active,
-.bottom-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .bottom-reverse-enter-from,
 .bottom-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(0, v-bind(translateY), 0);
 }
 
-.bottom-enter-active,
-.bottom-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .bottom-enter-from {
-  opacity: 0;
   transform: translate3d(0, v-bind(translateY), 0);
 }
 .bottom-leave-to {
-  opacity: 0;
   transform: translate3d(0, calc(v-bind(translateY) * -1), 0);
 }
 
-.left-reverse-enter-active,
-.left-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .left-reverse-enter-from,
 .left-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), 0, 0);
 }
 
-.left-enter-active,
-.left-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .left-enter-from {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), 0, 0);
 }
 .left-leave-to {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), 0, 0);
 }
 
-.right-reverse-enter-active,
-.right-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .right-reverse-enter-from,
 .right-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), 0, 0);
 }
 
-.right-enter-active,
-.right-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .right-enter-from {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), 0, 0);
 }
 .right-leave-to {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), 0, 0);
 }
 
-.top-left-reverse-enter-active,
-.top-left-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .top-left-reverse-enter-from,
 .top-left-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), calc(v-bind(translateY) * -1), 0);
 }
 
-.top-left-enter-active,
-.top-left-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .top-left-enter-from {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), calc(v-bind(translateY) * -1), 0);
 }
 .top-left-leave-to {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), v-bind(translateY), 0);
 }
 
-.top-right-reverse-enter-active,
-.top-right-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .top-right-reverse-enter-from,
 .top-right-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), calc(v-bind(translateY) * -1), 0);
 }
 
-.top-right-enter-active,
-.top-right-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .top-right-enter-from {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), calc(v-bind(translateY) * -1), 0);
 }
 .top-right-leave-to {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), v-bind(translateY), 0);
 }
 
-.bottom-left-reverse-enter-active,
-.bottom-left-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .bottom-left-reverse-enter-from,
 .bottom-left-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), v-bind(translateY), 0);
 }
 
-.bottom-left-enter-active,
-.bottom-left-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .bottom-left-enter-from {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), v-bind(translateY), 0);
 }
 .bottom-left-leave-to {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), calc(v-bind(translateY) * -1), 0);
 }
 
-.bottom-right-reverse-enter-active,
-.bottom-right-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .bottom-right-reverse-enter-from,
 .bottom-right-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), v-bind(translateY), 0);
 }
 
-.bottom-right-enter-active,
-.bottom-right-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .bottom-right-enter-from {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), v-bind(translateY), 0);
 }
 .bottom-right-leave-to {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), calc(v-bind(translateY) * -1), 0);
 }
 </style>
--- src/components/_transition/Zoom.vue
+++ src/components/_transition/Zoom.vue
@@ -31,7 +31,7 @@ interface TransitionProps {
 const {
   direction = 'zoom',
   appear = false,
-  duration = '350ms',
+  duration = '300ms',
   easing = 'ease-in-out',
   delay = '0ms',
   translateX = '100%',
@@ -47,215 +47,134 @@ const {
   </Transition>
 </template>
 
-<style>
-.zoom-enter-active,
-.zoom-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
+<style scoped>
+[class*='-enter-active'],
+[class*='-leave-active'] {
+  transition:
+    transform v-bind(duration) v-bind(easing) v-bind(delay),
+    opacity v-bind(duration) v-bind(easing) v-bind(delay);
 }
+
+[class*='-enter-from'],
+[class*='-leave-to'] {
+  opacity: 0;
+}
+
 .zoom-enter-from,
 .zoom-leave-to {
-  opacity: 0;
   transform: scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.top-reverse-enter-active,
-.top-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .top-reverse-enter-from,
 .top-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(0, calc(v-bind(translateY) * -1), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.top-enter-active,
-.top-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .top-enter-from {
-  opacity: 0;
   transform: translate3d(0, calc(v-bind(translateY) * -1), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 .top-leave-to {
-  opacity: 0;
   transform: translate3d(0, v-bind(translateY), 0) scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.bottom-reverse-enter-active,
-.bottom-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .bottom-reverse-enter-from,
 .bottom-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(0, v-bind(translateY), 0) scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.bottom-enter-active,
-.bottom-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .bottom-enter-from {
-  opacity: 0;
   transform: translate3d(0, v-bind(translateY), 0) scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 .bottom-leave-to {
-  opacity: 0;
   transform: translate3d(0, calc(v-bind(translateY) * -1), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.left-reverse-enter-active,
-.left-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .left-reverse-enter-from,
 .left-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), 0, 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.left-enter-active,
-.left-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .left-enter-from {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), 0, 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 .left-leave-to {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), 0, 0) scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.right-reverse-enter-active,
-.right-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .right-reverse-enter-from,
 .right-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), 0, 0) scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.right-enter-active,
-.right-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .right-enter-from {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), 0, 0) scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 .right-leave-to {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), 0, 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.top-left-reverse-enter-active,
-.top-left-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .top-left-reverse-enter-from,
 .top-left-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), calc(v-bind(translateY) * -1), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.top-left-enter-active,
-.top-left-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .top-left-enter-from {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), calc(v-bind(translateY) * -1), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 .top-left-leave-to {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), v-bind(translateY), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.top-right-reverse-enter-active,
-.top-right-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .top-right-reverse-enter-from,
 .top-right-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), calc(v-bind(translateY) * -1), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.top-right-enter-active,
-.top-right-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .top-right-enter-from {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), calc(v-bind(translateY) * -1), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 .top-right-leave-to {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), v-bind(translateY), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.bottom-left-reverse-enter-active,
-.bottom-left-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .bottom-left-reverse-enter-from,
 .bottom-left-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), v-bind(translateY), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.bottom-left-enter-active,
-.bottom-left-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .bottom-left-enter-from {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), v-bind(translateY), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 .bottom-left-leave-to {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), calc(v-bind(translateY) * -1), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.bottom-right-reverse-enter-active,
-.bottom-right-reverse-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .bottom-right-reverse-enter-from,
 .bottom-right-reverse-leave-to {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), v-bind(translateY), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 
-.bottom-right-enter-active,
-.bottom-right-leave-active {
-  transition: all v-bind(duration) v-bind(easing) v-bind(delay);
-}
 .bottom-right-enter-from {
-  opacity: 0;
   transform: translate3d(v-bind(translateX), v-bind(translateY), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
 .bottom-right-leave-to {
-  opacity: 0;
   transform: translate3d(calc(v-bind(translateX) * -1), calc(v-bind(translateY) * -1), 0)
     scale3d(v-bind(scaleX), v-bind(scaleY), 0);
 }
