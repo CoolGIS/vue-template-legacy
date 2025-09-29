aae83e84fd0359d36d0797c894017eb4fdb7e9f1
haoj <haoj@geoscene.com.cn>
2025-09-29 18:52:21 +0800

    feat: 修复判断非保护路由逻辑
    (cherry picked from commit dd537dcfde2c846e59000361b8f0ec01479dc0f6)

--- src/plugins/fetch/config.ts
+++ src/plugins/fetch/config.ts
@@ -1,11 +1,11 @@
-const UNPROTECTED_PATHS = ['/login', '/public']
+const UNPROTECTED_ROUTE_PREFIXES = ['/api/public/', '/api/auth/login', '/api/auth/register']
 
 function getToken() {
   return localStorage.getItem('token')
 }
 
 function isUnprotectedPath(pathname: string) {
-  return UNPROTECTED_PATHS.includes(pathname)
+  return UNPROTECTED_ROUTE_PREFIXES.some((prefix) => pathname.startsWith(prefix))
 }
 
 export { getToken, isUnprotectedPath }
