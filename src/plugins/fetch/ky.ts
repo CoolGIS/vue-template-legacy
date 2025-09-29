e6fcfd9b2b2bbdf23e3a0e1c652776f7ece78ce3
haoj <haoj@geoscene.com.cn>
2025-09-29 18:53:52 +0800

    feat: 增加非标准rest接口适配
    (cherry picked from commit 33cd8bf8e0b1e6fd7073aa84acf6d989bf635e91)

--- src/plugins/fetch/ky.ts
+++ src/plugins/fetch/ky.ts
@@ -1,6 +1,15 @@
 import ky from 'ky'
 import { getToken, isUnprotectedPath } from '@/plugins/fetch/config.ts'
 
+export class APIError extends Error {
+  readonly code: number
+  constructor(code: number, message: string) {
+    super(message)
+    this.name = 'APIError'
+    this.code = code
+  }
+}
+
 const api = ky.create({
   prefixUrl: import.meta.env.VITE_API_BASE_URL,
   retry: 0,
@@ -17,6 +26,13 @@ const api = ky.create({
     ],
     afterResponse: [
       async (_request, { zodSchema }, response) => {
+        // 非标准接口判断是否出错
+        /* const res= await response.clone().json()
+
+        if(res.code !== 200) {
+          throw new APIError(res.code, res.message)
+        } */
+
         // 数据校验
         if (zodSchema) {
           const data = await response.clone().json()
